ca08426564d9e30a1fcf2b7bdbb42ccf3d493184
sailself <sailself2008@gmail.com>
2023-04-13 17:45:30 -0700

    Change audio handler to transcribe audio longer than 30 seconds.

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# telegram_bot_whisper
+# Telegram Audio Transcription Bot
+
+This is a Telegram bot that uses the Whisper library to transcribe audio messages. It can transcribe both audio and voice messages in multiple languages.
+
+## Installation
+
+To use this bot, you need to have Python 3.x installed on your machine. You also need to install the `openai-whisper` library:
+
+```bash
+pip install -U openai-whisper
+```
+
+## Usage
+
+1. Clone the repository and navigate to the root folder.
+2. Create a new file called `conf.ini` and add the following content:
+
+   ```ini
+   [Telegram]
+   BOT_TOKEN=<your-bot-token>
+   ```
+
+   Replace `<your-bot-token>` with your Telegram bot token.
+3. Start the bot by running the following command:
+
+   ```bash
+   python whisper_telegram_bot.py
+   ```
+
+   The bot will start listening for incoming messages.
+4. Send an audio or voice message to the bot, and it will transcribe the message and send the result back to you.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Acknowledgments
+
+This project uses the following open source libraries:
+
+- [OpenAI Whisper](https://github.com/openai/whisper): for speech recognition and language detection.
+- [python-telegram-bot](https://github.com/python-telegram-bot/python-telegram-bot): for building the Telegram bot.
--- whisper_telegram_bot.py
+++ whisper_telegram_bot.py
@@ -1,8 +1,12 @@
 import os
-os.system("pip install -U openai-whisper")
+# os.system("pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118")
+# os.system("pip install -U openai-whisper")
+# os.system("pip install -U pyTelegramBotAPI")
 import telebot
+from telebot import types
 import configparser
 import whisper
+import datetime
 
 # Load bot token from conf.ini file
 config = configparser.ConfigParser()
@@ -10,15 +14,52 @@ config.read('conf.ini')
 BOT_TOKEN=config['Telegram']['BOT_TOKEN']
 
 # Load model
-model = whisper.load_model("medium")
+model = whisper.load_model("small")
 
 # Create bot
 bot = telebot.TeleBot(BOT_TOKEN, parse_mode=None)
 
+# Create bot commands
+bot.set_my_commands([{'command': 'start', 'description': 'Start the bot'}, {'command': 'help', 'description': 'Help'}])
+
+# Define the menu options
+menu_options = ['English', 'Spanish', 'French', 'German', 'Italian', 'Portuguese', 'Russian', 'Japanese', 'Korean', 'Chinese']
+
+# Define the function that will handle the menu request
+def handle_menu(callback_query):
+	# Get the message from the callback query
+	message = callback_query.message
+	# Get the user from the callback query
+	user = callback_query.from_user
+	# Get the data from the callback query
+	data = callback_query.data
+	# Get the message id from the callback query
+	message_id = message.message_id
+	# Get the chat id from the callback query
+	chat_id = message.chat.id
+	# Get the message text from the callback query
+	message_text = message.text
+	# Get the message date from the callback query
+	message_date = message.date
+	bot.answer_callback_query(callback_query.id, text=f"You {user} have selected {data}")
+
+# Add the callback query handler for the menu options
+bot.callback_query_handler(func=handle_menu)	
+
+# Handle /menu command
+@bot.message_handler(commands=['menu'])
+def send_menu(message):
+	markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+	markup.add(*menu_options)
+	bot.reply_to(message, "Select a language:", reply_markup=markup)
+
 # Handle /start and /help
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "This is a bot that uses the Whisper library to transcribe audio messages.")
+	bot.reply_to(message, "This is a bot that uses the Whisper AI model to transcribe audio messages.\n"+
+						"You can send an audio file or a voice message to the bot and it will transcribe it.\n"+
+						"Note that the bot is still in development and may not work properly.\n"+
+						"Voice messages are transcribed faster than audio files. But with 30 seconds length limit.")
         
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])		
 @bot.message_handler(func=lambda m: True)
@@ -30,29 +71,31 @@ def echo_all(message):
 def handle_audio(message):
 	file_info = bot.get_file(message.audio.file_id)
 	downloaded_file = bot.download_file(file_info.file_path)
-	with open("audio.ogg", 'wb') as new_file:
+	with open(f"{message.message_id}_audio.ogg", 'wb') as new_file:
 		new_file.write(downloaded_file)
-	audio = whisper.load_audio("audio.ogg")
-	audio = whisper.pad_or_trim(audio)
-	mel = whisper.log_mel_spectrogram(audio).to(model.device)
-	_, probs = model.detect_language(mel)
-	options = whisper.DecodingOptions(fp16 = False)
-	result = whisper.decode(model, mel, options)
-	bot.reply_to(message, result.text)
+	# Log current time	
+	print(f"Transcribing audio from message {message.message_id} start time {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+	# Transcribe audio with model.transcribe() which has no length limit
+	result = model.transcribe(f"{message.message_id}_audio.ogg", fp16=False)
+	print(f"Transcribing audio from message {message.message_id} finish time {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+	bot.reply_to(message, result["text"])
 
 # Handle all voice messages
 @bot.message_handler(content_types=['voice'])
 def handle_voice(message):
 	file_info = bot.get_file(message.voice.file_id)
 	downloaded_file = bot.download_file(file_info.file_path)
-	with open("voice.ogg", 'wb') as new_file:
+	with open(f"{message.message_id}_voice.ogg", 'wb') as new_file:
 		new_file.write(downloaded_file)
-	audio = whisper.load_audio("voice.ogg")
+	print(f"Transcribing voice from message {message.message_id} start time {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+	audio = whisper.load_audio(f"{message.message_id}_voice.ogg")
 	audio = whisper.pad_or_trim(audio)
 	mel = whisper.log_mel_spectrogram(audio).to(model.device)
 	_, probs = model.detect_language(mel)
+	print(f"Detected language: {max(probs, key=probs.get)}")
 	options = whisper.DecodingOptions(fp16 = False)
 	result = whisper.decode(model, mel, options)
+	print(f"Transcribing voice from message {message.message_id} finish time {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 	bot.reply_to(message, result.text)
 
 # Run bot
